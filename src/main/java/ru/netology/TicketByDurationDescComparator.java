380f4953d8d001accf51cf2539cce53df1df3ece
LuNTIK969 <nasa.969@gmail.com>
2023-03-07 23:24:21 +0300

    Comparator fixed

--- src/main/java/ru/netology/TicketByDurationDescComparator.java
+++ src/main/java/ru/netology/TicketByDurationDescComparator.java
@@ -1,8 +1,9 @@
 package ru.netology;
 
+import java.io.Serializable;
 import java.util.Comparator;
 
-public class TicketByDurationDescComparator implements Comparator<Ticket> {
+public class TicketByDurationDescComparator implements Comparator<Ticket>, Serializable {
 
     @Override
     public int compare(Ticket o1, Ticket o2) {
